3471ee7af2187ffa815206de2ab2568928783fb9
0xkenta <kenta0558kiichi614@gmail.com>
2024-02-04 12:54:35 +0900

    install the uniswap/permit2

--- src/Verifier.sol
+++ src/Verifier.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Verifer {}
+import {IPermit2} from "permit2/interfaces/IPermit2.sol";
+
+contract Verifier {
+    IPermit2 public permit2;
+
+    constructor(address _permit2) {
+        permit2 = IPermit2(_permit2);
+    }
+
+    function execute(
+        ISignatureTransfer.PermitBatchTransferFrom memory permit,
+        ISignatureTransfer.SignatureTransferDetails[] calldata transferDetails,
+        address owner,
+        bytes32 witness,
+        string calldata witnessTypeString,
+        bytes calldata signature
+    ) external {
+        // permit2.permitWitnessTransferFrom(permit, transferDetails, owner, witness, witnessTypeString, signature);
+    }
+}
--- test/Verifier.sol
+++ test/Verifier.sol
@@ -2,18 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Verifer} from "../src/Verifer.sol";
+import {Verifier} from "../src/Verifier.sol";
+import {IPermit2} from "permit2/interfaces/IPermit2.sol";
 
 contract VeriferTest is Test {
-    Verifer public verifer;
+    Verifier public verifier;
 
-    IPermit2 public permit2;
+    address public permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+
+    uint256 public DEFAULT_FEE_AMOUNT = 1 ether;
 
     function setUp() public {
-        verifer = new Verifer();
+        verifier = new Verifier(permit2);
     }
 
     function test_initialize() public {
-        assertEq(address(verifer.permit2()), address(permit2));
+        assertEq(address(verifier.permit2()), permit2);
+    }
+
+    function test_execute() public {
+        uint256 nonce = 0;
     }
 }
